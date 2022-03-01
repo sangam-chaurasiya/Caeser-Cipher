6857b0bf209898c9662271717df869c9d782afe1
Sangam Chaurasiya <76783397+sangam-chaurasiya@users.noreply.github.com>
2022-03-01 16:22:57 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesure(message, shift_amount, convert_format):
+    new_message = ""
+    if convert_format.lower() == 'decode':
+        shift_amount *= -1
+    for character in message:
+        if character not in alphabet:
+            new_message += character
+        else:
+            encrypted_character_index = alphabet.index(character) + shift_amount
+            new_message += alphabet[encrypted_character_index]
+    return new_message
+
+
+# Printing logo
+print(logo)
+
+while True:
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n")) % 26
+
+    print(f"Here's the {direction}d result: {caesure(message=text, shift_amount=shift, convert_format=direction)}")
+
+    operation = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if operation.lower() == 'no':
+        print("Good Bye")
+        break
